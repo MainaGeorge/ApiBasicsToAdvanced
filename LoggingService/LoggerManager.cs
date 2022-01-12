87cb4e9ecaca67bbb67b46d6f9e497cbd4029be6
George Maina <=>
2022-01-12 18:24:27 +0100

    wired repository context to use sql server

---
+++ LoggingService/LoggerManager.cs
@@ -0,0 +1,32 @@
+﻿using Contracts;
+using NLog;
+
+namespace LoggingService
+{
+    public class LoggerManager : ILoggerManager
+    {
+        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
+        public LoggerManager()
+        {
+        }
+        public void LogInfo(string message)
+        {
+            Logger.Info(message);
+        }
+
+        public void LogWarning(string message)
+        {
+            Logger.Warn(message);
+        }
+
+        public void LogDebug(string message)
+        {
+            Logger.Debug(message);
+        }
+
+        public void LogError(string message)
+        {
+            Logger.Error(message);
+        }
+    }
+}
