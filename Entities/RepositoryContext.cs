67bf130ce7f7a9645bc5c56335fb62d587ff16b8
George Maina <=>
2022-01-13 07:53:21 +0100

    seeded initial data

--- Entities/RepositoryContext.cs
+++ Entities/RepositoryContext.cs
@@ -5,12 +5,19 @@ namespace Entities
 {
     public class RepositoryContext : DbContext
     {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Company> Companies { get; set; }
+
         public RepositoryContext(DbContextOptions<RepositoryContext> options) : base(options)
         {
         }
 
-        public DbSet<Employee> Employees { get; set; }
-        public DbSet<Company> Companies { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CompanyConfiguration());
+            modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
     }
 
 }
